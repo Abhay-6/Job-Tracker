ad2892431f6e6fb5920bfffd3bbb8d087d9db5e4
Abhay Kumar <abhayjha78588@gmail.com>
2026-09-08 00:11:26 +0530

    fix backend authentication and cors

--- Backend/app.js
+++ Backend/app.js
@@ -7,17 +7,11 @@ import {middleware } from "./middlewares/error.middlewares.js";
 
 const app=express();
 
-app.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "Job Tracker Backend is running"
-    });
-});
-
 app.use(cors({
     origin: "https://job-tracker-phvz.vercel.app",
     credentials:true
 }))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
